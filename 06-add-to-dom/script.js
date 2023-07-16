c8e60d421231683459adcaabd615b8128c59468a
siaprincessm <siaprincessm@gmail.com>
2023-07-16 11:48:06 +0000

    a0-add-to dom complete

--- 06-add-to-dom/script.js
+++ 06-add-to-dom/script.js
@@ -1,1 +1,8 @@
 console.log("Hello World!");
+
+let btn = document.querySelector('#randomNumberButton')
+    btn.addEventListener('click', function(){
+    let randomNumber = Math.floor(Math.random() * 1000 + 1);
+    let randomResult = document.querySelector('#result');
+        randomResult.innerHTML += '<li>' + randomNumber + '</li>';
+    })
