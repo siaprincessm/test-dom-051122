8697325d377bf11cedaade6b1a15bf674d7e251f
siaprincessm <siaprincessm@gmail.com>
2023-08-02 03:58:59 +0000

    form validation finished

--- 07-form-validation-2/script.js
+++ 07-form-validation-2/script.js
@@ -15,15 +15,41 @@ let btn = document.querySelector('#submitBtn');
             if (email.includes('@') == false || email.includes('.') == false ){
                 emailIsInvalid = true;
             }
+    //TO CHECK THE PASSWORD
+            let password = document.querySelector('#password').value;
+                if (password.length < 10) {
+                    passwordTooShort = true;
+                }
     //TO SELECT THE ERROR ELEMENT INSIDE THE HTML FILE
         let errorDiv = document.querySelector('#errors')
+
+    //TO CLEAR EXISTING ERROR MESSAGE
+        errorDiv.innerHTML = '';
+
+        let errorContainers = document.querySelectorAll('.error');
+            for (let e of errorContainers){
+                e.innerHTML = "";
+            }
 
     //TO INITIALIZE THE ERROR MESSAGES
             if (usernameTooShort) {
                 errorDiv.innerHTML += "Username too short";
+                document.querySelector('#error-username').innerHTML += "Username is at least 10 characters";
             }
             if (emailIsInvalid){
                 errorDiv.innerHTML += "Email must include @ and .";
+                document.querySelector('#error-email').innerHTML += "Email is invalid format";
+            } 
+            if (passwordTooShort) {
+                errorDiv.innerHTML += "Password is too short";
+                document.querySelector('#error-password').innerHTML += "Password invalid";
             }
-            alert(errorMsg);
+    //TO CHECK FOR ERROR MESSAGE
+            if (usernameTooShort || emailIsInvalid || passwordTooShort) {
+                errorDiv.style.display = 'block';
+            } else {
+    //IF NO ERRORS HIDE THE ERROR MESSAGE
+            errorDiv.style.display = 'none';
+            alert("Submission successful")
+            }
   })
